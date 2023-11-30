35a92cafcef56686a929337d6c5d8fc5700da071
Zach <djessitokoto@gmail.com>
2023-11-30 16:55:31 +0100

    chesslogique

--- src/components/Chessboard.jsx
+++ src/components/Chessboard.jsx
@@ -1,7 +1,16 @@
-const chessBoard = () => (
-  <div className="flex mx-6 bg-black">
-    
-</div>
-)
+const chessBoard = () => {
+  const rows = 8
+  const cols = 8
+  const board = []
 
+  for (let i = 0; i < rows; i += 1) {
+    board.push([])
+    for (let j = 0; j < cols; j +=1) {
+      board[i].push(null)
+    }
+  }
+  
+
+  return board
+}
 export default chessBoard
