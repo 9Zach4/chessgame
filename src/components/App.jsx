2ad1c66e91f8641e0e3acf2f3d8991c1a5012420
Zach <djessitokoto@gmail.com>
2023-11-30 15:41:09 +0100

    chessboard init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,16 @@
+import React from "react"
+import Chessboard from "./Chessboard"
+
 const App = () => (
-  
+  <div>
     <div className="flex justify-center bg-black py-5">
       <h1 className="text-white text-xl font-bold">Chess Game</h1>
     </div>
-    
+
+    <div className="flex justify-center m-5 bg-black py-5 bg-orange-500	">
+        <Chessboard /> 
+    </div>
+  </div>
 )
-  
 
 export default App
---
+++ src/components/Chessboard.jsx
@@ -0,0 +1,7 @@
+const chessBoard = () => (
+  <div className="flex mx-6 bg-black">
+    
+</div>
+)
+
+export default chessBoard
