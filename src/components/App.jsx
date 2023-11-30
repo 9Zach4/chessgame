deba2cd90ae4287e9ebf0ed7f982a8a0ce604eff
Zach <djessitokoto@gmail.com>
2023-11-30 15:43:10 +0100

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,6 @@ const App = () => (
     <div className="flex justify-center bg-black py-5">
       <h1 className="text-white text-xl font-bold">Chess Game</h1>
     </div>
-
     <div className="flex justify-center m-5 bg-black py-5 bg-orange-500	">
         <Chessboard /> 
     </div>
