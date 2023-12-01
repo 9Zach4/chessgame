864707cb0050234f80ed2e29fd2ecffe77ae2c79
Zach <djessitokoto@gmail.com>
2023-12-01 12:12:42 +0100

    chessboard OK

--- src/components/Chessboard.jsx
+++ src/components/Chessboard.jsx
@@ -1,42 +1,20 @@
-const chessBoard = () => {
-  const rows = Array(8).fill().map((x, i) => 8 - i)
-  const cols = Array(8).fill().map((x, i) => i)
-  // Const board = []
+import React from "react"
+import { chessGrid } from "./constant"
 
-  return (
-    <div className="grid grid-cols-8">
-      <div className="bg-white"></div>
-      {rows.map((row) => (
-        cols.map((col) => (
-          <div key={row + col} className={`p-4 ${row + col % 2 === 0 ? "bg-white" : "bg-white"}` >
-            {row}
-            {col}
-          </div>
-          
-        ))
-      ))}
-    </div>
-  )
-}
-  
-       // For (let i = 0; i < rows.lenght; i += 1) {
-      
-  //   const row = rows[i]
-  //   for (let j = 0; j < cols.lenght; j += 1) {
-  //     const col = cols[j]
-  //     board.push({ row, col })
-  //   }
-  // }
-  
-  // return (
-  //   <div className="grid grid-col-8 bg-black">
-  //     {board.map((square) => (
-  //       <div key={square.row + square.col}>
-  //         {square.row}
-  //         {square.col}
-  //       </div>
-  //     ))}
-  //   </div>
-  
+const ChessBoard = () => (
+  <div className="grid grid-cols-16">
+    {chessGrid.map((row, rowIndex) => (
+      <div key={rowIndex} className="flex">
+        {row.map((col, colIndex) => (
+          <div
+            key={col}
+            className={`h-12 w-12 ${(rowIndex+colIndex) % 2 === 0 ? "bg-white" : "bg-black"}`}
+          ></div>
+        ))}
+        
+      </div>
+    ))}
+  </div>
+)
 
-export default chessBoard
+export default ChessBoard
