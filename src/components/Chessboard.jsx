e9e19376b4195bc472b445963164894f2e817212
Zach <djessitokoto@gmail.com>
2023-12-01 16:44:06 +0100

    interface OK

--- src/components/Chessboard.jsx
+++ src/components/Chessboard.jsx
@@ -1,20 +1,24 @@
 import React from "react"
-import { chessGrid } from "./constant"
+import { chessGrid, piece } from "./data"
+
 
 const ChessBoard = () => (
-  <div className="grid grid-cols-16">
-    {chessGrid.map((row, rowIndex) => (
+  <div className="border p-5 border-dashed border-white border-4">
+   <div className="border border-solid border-black border-1">
+   <div className="grid grid-cols-16">
+     {chessGrid.map((row, rowIndex) => (
       <div key={rowIndex} className="flex">
         {row.map((col, colIndex) => (
           <div
             key={col}
             className={`h-12 w-12 ${(rowIndex+colIndex) % 2 === 0 ? "bg-white" : "bg-black"}`}
-          ></div>
+          >{piece}</div>
         ))}
-        
       </div>
     ))}
-  </div>
+     </div>
+     </div>
+    </div>
 )
 
 export default ChessBoard
