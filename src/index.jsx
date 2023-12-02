7cc06317aa0676010fa518281240a2a1e46a90a9
Zach <djessitokoto@gmail.com>
2023-12-02 13:09:10 +0100

    app

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import App from "./compoentns/app"
+import App from "./components/App.jsx"
 
 const rootElement = document.querySelector("#app")
 const root = createRoot(rootElement)
